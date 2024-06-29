539ce9a1b5918a478dad2ea15a1baea304d37e75
Bishal221 <kayalbishal221@gmail.com>
2024-06-29 16:24:25 +0530

    Changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '91405784e0d3d10d06f6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '0a607876ba2af52eff8d');
